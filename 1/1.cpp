c0f9c2b08c94c603ef54ec2d3b6dd7cc331d9348
Rico Kölling <rico.koelling@uni-jena.de>
2021-04-24 01:45:18 +0200

    Task 1 finished(still need to put it in form on .txt file); Task2 finished to sub

--- 1/1.cpp
+++ 1/1.cpp
@@ -2,5 +2,16 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a = 5, b = 2;
+    double c = b;
+
+    double result1 = a * b - - - - - - 12.0f;
+    double result2 = a / b / c;
+    double result3 = a / c / b;
+    double result4 = -1.0f - a*2E-1 + a / 2;
+    double result5 = 1.0 + (a += (2 / - b - (c += .0E2)));
+
+    printf("%f %f %f %f %f", result1, result2, result3, result4, result5);
+
+    return 0; 
 }
