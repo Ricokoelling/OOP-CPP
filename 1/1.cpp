e4860f4fa5dfa80888621b347445bc41880ed5c9
Rico Kölling <rico.koelling@uni-jena.de>
2021-04-26 21:04:09 +0200

    Finished task 1.1 maybe reworking 1.2 if I got some time left

--- 1/1.cpp
+++ 1/1.cpp
@@ -9,7 +9,7 @@ int main(){
     double result2 = a / b / c;
     double result3 = a / c / b;
     double result4 = -1.0f - a*2E-1 + a / 2;
-    double result5 = 1.0 + (a += (2 / - b - (c += .0E2)));
+    double result5 = 1.0 + (a *= (2 / - b - (c += .0E2)));
 
     printf("%f %f %f %f %f", result1, result2, result3, result4, result5);
 
