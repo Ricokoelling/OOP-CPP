a8434c8486d47c2bb5559c6f804b626efc7c164a
Rico Kölling <rico.koelling@uni-jena.de>
2021-05-21 10:54:22 +0200

    Finished Worksheet 4, might change exception to coaught exception

---
+++ 4/1.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+double max(const vector<double> &v)
+{
+    double maximum = INT16_MIN;
+    for (double d : v)
+    {
+        if (maximum < d)
+        {
+            maximum = d;
+        }
+    }
+    return maximum;
+}
+bool allPositive(const vector<double> &v)
+{
+    for (double d : v)
+    {
+        if (d > 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+double product(const vector<double> &v1, const vector<double> &v2)
+{
+    double solution = 0;
+    for (int i = 0; i < v1.size() && v2.size(); i++)
+    {
+        solution += v1[i] * v1[i];
+    }
+    return solution;
+}
+vector<double> product(const vector<double> &v, double f)
+{
+    vector<double> solution;
+    for (int i = 0; i < v.size(); i++)
+    {
+        solution.push_back(v[i] * f);
+    }
+    return solution;
+}
+double norm(const vector<double> &v)
+{
+    int part = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        part += (v[i] * v[i]);
+    }
+    return sqrt(part);
+}
+void normalize(vector<double> &v)
+{
+    double temp = norm(v);
+    for (double &d : v)
+    {
+        d = d / temp;
+    }
+}
+
+int main()
+{
+    int choice;
+    vector<double> array1, array2, result;
+    int input, counter = 0;
+    double temp;
+    cout << "Please insert what function you need: " << endl;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        cout << "Please insert how many numbers your vector has: ";
+        cin >> input;
+        cin.ignore();
+        cout << "Please insert your numbers for the vector: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array1.push_back(temp);
+        }
+        cout << "The biggest number in the vector is: " << max(array1) << endl;
+        break;
+
+    case 2:
+        cout << "Please insert how many numbers your vector has: ";
+        cin >> input;
+        cout << "Please insert your numbers for the vector: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array1.push_back(temp);
+        }
+        if (allPositive(array1) == true)
+        {
+            cout << "The vector is positiv in al its numbers" << endl;
+        }
+        else
+            cout << "The vector is not positiv in al its numbers" << endl;
+        break;
+    case 3:
+        cout << "Please insert how many numbers your vector's got: ";
+        cin >> input;
+        cout << "Please insert your numbers for the vector1: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array1.push_back(temp);
+        }
+        cout << "Please insert your numbers for the vector2: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array2.push_back(temp);
+        }
+        cout << "The scalar product is: " << product(array1, array2) << endl;
+        break;
+    case 4:
+        cout << "Please insert how many numbers your vector has: ";
+        cin >> input;
+        cout << "Please insert your numbers for the vector: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array1.push_back(temp);
+        }
+        cout << "Please insert the number you want your vector to be multiplied with: ";
+        cin >> temp;
+        result = product(array1, temp);
+        cout << "The product is: " << endl;
+        for (int i = 0; i < result.size(); i++)
+        {
+            cout << result[i] << " ";
+        }
+        break;
+    case 5:
+        cout << "Please insert how many numbers your vector has: ";
+        cin >> input;
+        cout << "Please insert your numbers for the vector: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array1.push_back(temp);
+        }
+        cout << "The norm is: " << norm(array1) << endl;
+        break;
+    case 6:
+        cout << "Please insert how many numbers your vector has: ";
+        cin >> input;
+        cout << "Please insert your numbers for the vector: ";
+        for (int i = 0; i < input; i++)
+        {
+            cin >> temp;
+            array1.push_back(temp);
+        }
+        normalize(array1);
+        cout << "The normalised vector is: " << endl;
+        for (int i = 0; i < array1.size(); i++)
+        {
+            cout << array1[i] << " ";
+        }
+        break;
+    default:
+        cout << "Invalid input" << endl;
+        break;
+    }
+    return 0;
+}
---
+++ 4/2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+bool test(const char &c)
+{
+    if (c >= '0' && c <= '9' || c == ' ')
+    {
+        return true;
+    }
+    return false;
+}
+int convert(const string &s)
+{
+    int result = 0, temp;
+    bool check;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (!test(s[0]) || (!test(s[i + 1]) && test(s[i + 2])) || (!test(s[i + 1]) && i + 1 < s.size()))
+        {
+            cout << "Fehler!" << endl;
+            return -1;
+        }
+        if (s[i] >= '0' && s[i] <= '9' || s[i] == ' ' && !(s[i] >= '0' && s[i] <= '9'))
+        {
+            if (s[i] != ' ')
+            {
+                temp = s[i];
+                result += (temp - 48);
+                if (i + 1 < s.size() && s[i + 1] >= '0' && s[i + 1] <= '9')
+                    result *= 10;
+                else
+                    return result;
+            }
+        }
+        else
+            return result;
+    }
+    return result;
+}
+int main()
+{
+    string input;
+    cout << "Please insert your text: " << endl;
+    getline(cin, input);
+    cout << convert(input);
+
+    return 0;
+}
